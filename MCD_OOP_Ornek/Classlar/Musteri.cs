afc0463c4ce38da3185cd107aa6e27a2d399210d
Esra Alkan <es.alknn@gmail.com>
2021-11-29 16:27:57 +0300

    MusteriSepet

--- MCD_OOP_Ornek/Classlar/Musteri.cs
+++ MCD_OOP_Ornek/Classlar/Musteri.cs
@@ -36,7 +36,22 @@ namespace MCD_OOP_Ornek.Classlar
 
         public void MusterininSepetiniYazdir()
         {
-
+            
+            if (MusterininUrunSepeti.UrunlerListesi.Count>0)
+            {
+                //Listeyi yazdırmadan önce doğum günü durumuna bakmanız gerekiyor.
+                if (MusterininDogumGunuMu() == true)
+                {
+                    MusterininUrunSepeti.DogumGunuHediyesiEkle();
+                    Console.WriteLine("Doğum gününüz kutlu olsun. Gofret hediyemiz sepete eklendi...");
+                }
+                int sayac = 1;
+                foreach (var item in MusterininUrunSepeti.UrunlerListesi)
+                {
+                    Console.WriteLine(sayac + " . ürününüz: " +item.UrunAdi);
+                    sayac++;
+                }
+            }
         }
 
         #endregion
--- MCD_OOP_Ornek/Program.cs
+++ MCD_OOP_Ornek/Program.cs
@@ -10,6 +10,69 @@ namespace MCD_OOP_Ornek
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Classlar.Urun u1 = new Classlar.Urun()
+                {
+                    UrunAdi = "Ekmek",
+                    UrunId = 1000,
+                    UrunKategorisi = UrunKategorileri.Gıda
+
+                };
+                Classlar.Urun u2 = new Classlar.Urun()
+                {
+                    UrunAdi = "Şarj Aleti",
+                    UrunId = 1001,
+                    UrunKategorisi = UrunKategorileri.Elektronik
+
+                };
+                Classlar.Urun u3 = new Classlar.Urun()
+                {
+                    UrunAdi = "Süt",
+                    UrunId = 1002,
+                    UrunKategorisi = UrunKategorileri.Gıda
+
+                };
+                Classlar.Urun u4 = new Classlar.Urun()
+                {
+                    UrunAdi = "Şampuan",
+                    UrunId = 1003,
+                    UrunKategorisi = UrunKategorileri.Temizlik
+
+                };
+
+                Classlar.Musteri m = new Classlar.Musteri
+                {
+                    MusteriId = 1,
+                    Cinsiyeti = Cinsiyetler.Erkek,
+                    DogumTarihi = new DateTime(1978,11,29),
+                    MusteriAdi = "Faik",
+                    MusteriSoyadi = "Turan"
+                    
+                };
+
+                Classlar.UrunSepeti musterisepeti = new Classlar.UrunSepeti
+                {
+                    MusteriId = m.MusteriId,
+                    SepetId= 1
+                };
+
+                musterisepeti.UrunlerListesi.Add(u1);
+                musterisepeti.UrunlerListesi.Add(u2);
+                musterisepeti.UrunlerListesi.Add(u3);
+                musterisepeti.UrunlerListesi.Add(u4);
+
+                m.MusterininUrunSepeti = musterisepeti;//Önce sepeti ve sepete eklenecek ürünleri yukarıda aldım sonra o sepeti müşterime atadım.
+
+                m.MusteriBilgileriYazdir();
+                m.MusterininSepetiniYazdir();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                
+            }
+            Console.ReadKey();
         }
     }
 }
